cd11c22b41b812a460562b1b8ee6b5aa9370fbe8
Mayuresh Murudkar <mayureshmurudkar02@gmail.com>
2023-03-23 13:34:25 +0530

    carousel section and rating done

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
-
+import useFetch from "../../../hooks/useFetch";
+import Carousel from "../../../components/carousel/Carousel";
 const Trending = () => {
   const onTabChange = (tab) => {};
+  const [endPoint, setEndPoint] = useState("day");
+
+  const { data, loading } = useFetch(`/trending/all/${endPoint}`);
+
   return (
     <div className="carouselSection">
       <ContentWrapper>
         <span className="carouselTitle">Trending</span>
         <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} />
     </div>
   );
 };
